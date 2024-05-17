88c16d693aaac16a39fbbb9f4f990112008929d7
aditya109 <adikid1996@gmail.com>
2024-05-17 22:20:17 +0530

    testing commit

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("ok")
 		fmt.Fprintf(w, "pong")
 	})
 
